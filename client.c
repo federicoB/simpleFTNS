709bd0f930f84d5c151bb7f69a509255d3389bba
federicoB <federicobertani96@gmail.com>
2016-11-19 09:32:18 +0100

    Clened up client.c. Moved unused getMac routine to setter_increment.c

--- client.c
+++ client.c
@@ -16,53 +16,9 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
-
-/*int getMACaddress(uint8_t * MACaddress) {
-    int status = 1;
-    char buf[256];
-    FILE *fp = fopen("/sys/class/net/eth0/address", "rt");
-    memset(buf, 0, 256);
-    if (fp) {
-        if (fgets(buf, sizeof buf, fp) > 0) {
-            sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &MACaddress[0],
-                   &MACaddress[1], &MACaddress[2], &MACaddress[3], &MACaddress[4], &MACaddress[5]);
-            status = 0;
-        }
-        fclose(fp);
-    }
-    return status;
-}*/
-
 int main(int argc, char **argv)
 {
-/*    set(12,12);
-    set(15,15);*/
-    /*int	clientSocket, numberOfCharacterRead;
-    uint8_t MACaddress;
-    struct sockaddr_in serverAddress;
-    uint64_t* buffer = malloc(sizeof(uint64_t));
-
-    if (argc != 2){
-        printf("You need to specify an ip address\n");
-        exit(-1);
-    }
-
-
-
-
-
-    if (getMACaddress(MACaddress)) {
-        printf("An error occurred while retrieving the MAC address");
-        exit(-4);
-    }
-
-
-    //TODO send syn packet with MAC address
-
-    while ( (numberOfCharacterRead = read(clientSocket, buffer,(ssize_t) sizeof(uint64_t))) > 0) {
-        //TODO check response
-    }*/
-
+    set(12,12);
+    set(15,15);
     return 0;
 }
--- setter_increment.c
+++ setter_increment.c
@@ -33,7 +33,23 @@ int client_Socket; //this is global for a reason
 struct sockaddr_in serverAddress;
 int token;
 
-int socketConnected(int* clientSocket) {
+/*int getMACaddress(uint8_t * MACaddress) {
+    int status = 1;
+    char buf[256];
+    FILE *fp = fopen("/sys/class/net/eth0/address", "rt");
+    memset(buf, 0, 256);
+    if (fp) {
+        if (fgets(buf, sizeof buf, fp) > 0) {
+            sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &MACaddress[0],
+                   &MACaddress[1], &MACaddress[2], &MACaddress[3], &MACaddress[4], &MACaddress[5]);
+            status = 0;
+        }
+        fclose(fp);
+    }
+    return status;
+}*/
+
+int socketConnected(int *clientSocket) {
     int error_code = 0;
     int error_code_size = sizeof(error_code);
     int returnValue = getsockopt(*clientSocket, SOL_SOCKET, SO_ERROR, &error_code, &error_code_size);
@@ -42,44 +58,43 @@ int socketConnected(int* clientSocket) {
 
 int initializeToken(int *token) {
     token = 0;
-    int result=1;
+    int result = 1;
     char buffer[PATH_MAX];
-    getcwd(buffer,PATH_MAX);
-    DIR* dir = opendir(buffer);
+    getcwd(buffer, PATH_MAX);
+    DIR *dir = opendir(buffer);
     //define a struct for containing current file
     struct dirent *directoryStruct;
     //while we haven't finish to read the files into the directory we count the number of files
     while ((directoryStruct = readdir(dir)) != NULL) {
-            #define filename directoryStruct->d_name
-            //it's ok to check only the first character
-            if (filename!='.') {
-                FILE* filetoken = fopen(filename,'r');
-                fread(&token,sizeof(token),1,filetoken);
-                fclose(filetoken);
-            }
+        #define filename directoryStruct->d_name
+        //it's ok to check only the first character
+        if (filename != '.') {
+            FILE *filetoken = fopen(filename, 'r');
+            fread(&token, sizeof(token), 1, filetoken);
+            fclose(filetoken);
+        }
     }
 }
 
-int establishSession(int* clientSocket) {
-    int result=1;
+int establishSession(int *clientSocket) {
+    int result = 1;
     //check if an existing open connection not exist
-    if (socketConnected(clientSocket)!=0) {
+    if (socketConnected(clientSocket) != 0) {
         if ((*clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-            result=-2;
+            result = -2;
         } else {
             memset(&serverAddress, 0, sizeof(serverAddress));
             serverAddress.sin_family = AF_INET;
             serverAddress.sin_port = htons(PORT);
             if (inet_pton(AF_INET, ADDRESS, &serverAddress.sin_addr) <= 0) {
-                result=-3;
+                result = -3;
             } else if (connect(*clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
-                result=-5;
+                result = -5;
             }
         }
-        if (result==1) {
+        if (result == 1) {
             //check if an existing token file is present. If present load it
             initializeToken(&token);
-            //create connection
             //sent Syn packet
             //wait success
         }
@@ -87,7 +102,7 @@ int establishSession(int* clientSocket) {
 }
 
 int set(uint32_t name, uint32_t value) {
-establishSession(&client_Socket);
+    establishSession(&client_Socket);
 //send setpacket
 //wait success
 }
