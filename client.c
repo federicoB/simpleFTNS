c1b16e27ccbd03a29ebbd0cb9c0f8cf8d1988c6f
federicoB <federicobertani96@gmail.com>
2016-11-22 18:09:07 +0100

    Started to create demo-client user menu.

--- client.c
+++ client.c
@@ -21,10 +21,41 @@
 
 int main(int argc, char **argv)
 {
-    set(12,1);
-    increment(12,42);
-    uint32_t  value;
-    get(12,&value);
-    printf("%u",value);
+    unsigned int response;
+    do {
+        printf("Choose an option\n");
+        printf("1-Set a new variable\n");
+        printf("2-Increase current variable\n");
+        printf("3-Get the value of a variable\n");
+        printf("4-Program exit\n");
+        scanf("%u", &response);
+        switch (response) {
+            case 1: {
+                uint32_t name, value;
+                printf("Insert the name of the new variable\n");
+                scanf("%u", &name);
+                printf("Insert the value of the new variable\n");
+                scanf("%u", &value);
+                set(name, value);
+                break;
+            }
+            case 2: {
+                uint32_t name,value;
+                printf("Insert the name of the variable to increase\n");
+                scanf("%u", &name);
+                printf("Insert how much to increase\n");
+                scanf("%u", &value);
+                increment(name,value);
+                break;
+            }
+            case 3: {
+                uint32_t name,value;
+                printf("Insert the name of the variable\n");
+                scanf("%u", &name);
+                get(name,&value);
+                printf("The value of the variable %u is %u\n",name,value);
+            }
+        }
+    }while (response!=4);
     return 0;
 }
