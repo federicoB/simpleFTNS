3ca6ad23fe5d63e4672bea42c58719eedacc67f7
federicoB <federicobertani96@gmail.com>
2016-11-22 18:27:09 +0100

    Added signatures at the beggining of setter_increment.c Re-ordered function based on abstraction level. Removed unused assignments.

--- setter_increment.c
+++ setter_increment.c
@@ -36,90 +36,61 @@ int client_Socket; //this is global for a reason
 struct sockaddr_in serverAddress;
 uint32_t token;
 
-/*int getMACaddress(uint8_t * MACaddress) {
-    int status = 1;
-    char buf[256];
-    FILE *fp = fopen("/sys/class/net/eth0/address", "rt");
-    memset(buf, 0, 256);
-    if (fp) {
-        if (fgets(buf, sizeof buf, fp) > 0) {
-            sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &MACaddress[0],
-                   &MACaddress[1], &MACaddress[2], &MACaddress[3], &MACaddress[4], &MACaddress[5]);
-            status = 0;
+int socketConnected(int *clientSocket);
+int initializeToken(uint32_t *token);
+int saveTokenToFile(uint32_t token);
+int sendPacket(SimpleProtocolPacket* packet;
+int waitResponse(SimpleProtocolPacket *responsePacket);
+int checkSuccess(SimpleProtocolPacket *responsePacket);
+int establishSession(int *clientSocket);
+
+int set(uint32_t name, uint32_t value) {
+    int result;
+    if ((result=establishSession(&client_Socket))){
+        //send set packet
+        SimpleProtocolPacket packet;
+        SPPINIT(packet);
+        SETSET(packet);
+        SETVAR(packet,name);
+        SETVAL(packet,value);
+        if ((result=sendPacket(&packet)))
+            //reuse packet for response
+            result= checkSuccess(&packet);
+    };
+    return result;
+}
+
+int increment(uint32_t name, uint32_t value) {
+    int result;
+    if ((result=establishSession(&client_Socket))){
+        //send increment packet
+        SimpleProtocolPacket packet;
+        SPPINIT(packet);
+        SETINC(packet);
+        SETVAR(packet,name);
+        SETVAL(packet,value);
+        if ((result=sendPacket(&packet)))
+            //reuse packet for response
+            result= checkSuccess(&packet);
+    };
+    return result;
+}
+
+int get(uint32_t name,uint32_t* value) {
+    int result;
+    if ((result=establishSession(&client_Socket))){
+        SimpleProtocolPacket packet;
+        SPPINIT(packet);
+        SETGET(packet);
+        SETVAR(packet,name);
+        if ((result=sendPacket(&packet))) {
+            //reuse packet for response
+            if ((result=checkSuccess(&packet))) {
+                if (GETSUC(packet)) *value = (uint32_t) GETVAL(packet);
+                else result=0;
+            };
         }
-        fclose(fp);
-    }
-    return status;
-}*/
-
-/**
- * Check if a given socket is already connected to a server or not.
- * @param clientSocket int*: the socket descriptor to check
- * @return 1 if already connected 0 if not
- */
-int socketConnected(int *clientSocket) {
-    //declare a variable for keeping getSockOpt error
-    int error_code = 0;
-    //get the size of error code variable
-    socklen_t error_code_size = sizeof(error_code);
-    //getting socket option of a not connected socket return -1
-    int returnValue = getsockopt(*clientSocket, SOL_SOCKET, SO_ERROR, &error_code, &error_code_size);
-    //1 if already connected 0 if not (!-1 is 0)
-    return (returnValue==-1);
-}
-
-int initializeToken(uint32_t *token) {
-    *token = 0;
-    int result = 1;
-    FILE* filetoken;
-    if (access(TOKEN_FILE_NAME, F_OK)==0) {
-        filetoken = fopen(TOKEN_FILE_NAME, "r");
-        if (filetoken != NULL) {
-            //read the token from file
-            if (fread(token, sizeof(*token), 1, filetoken) == 0) {
-                //if an error occur (fread return value == 0)
-                result = 0;
-            }
-            fclose(filetoken);
-        }
-    }
-    return result;
-}
-
-int saveTokenToFile(uint32_t token) {
-    int result = 1;
-    FILE* filetoken;
-        if ((filetoken=fopen(TOKEN_FILE_NAME,"w"))!=NULL) {
-            if (fwrite(&token,sizeof(token),1,filetoken)==0) {
-                    //if an error occur (fwrite return value == 0)
-                    result = 0;
-                }
-            } else result = 0;
-    return result;
-}
-
-int sendPacket(SimpleProtocolPacket* packet) {
-    int result=1;
-    *packet = SPPTONTW(*packet);
-    if (send(client_Socket,packet,sizeof(SimpleProtocolPacket),0)==-1) {
-        result = 0;
-    }
-    return result;
-}
-
-int waitResponse(SimpleProtocolPacket *responsePacket) {
-    int result=1;
-    if (recv(client_Socket,responsePacket,sizeof(SimpleProtocolPacket),0)!=-1) {
-        *responsePacket = SPPTOHST(*responsePacket);
-    } else result = 0;
-    return result;
-}
-
-int checkSuccess(SimpleProtocolPacket *responsePacket) {
-    int result=1;
-    if (waitResponse(responsePacket)) {
-        if (!GETSUC(*responsePacket)) result=0;
-    }
+    };
     return result;
 }
 
@@ -162,53 +133,90 @@ int establishSession(int *clientSocket) {
     return result;
 }
 
-int set(uint32_t name, uint32_t value) {
-    int result = 1;
-    //TODO check error checking
-    if ((result=establishSession(&client_Socket))){
-        //send set packet
-        SimpleProtocolPacket packet;
-        SPPINIT(packet);
-        SETSET(packet);
-        SETVAR(packet,name);
-        SETVAL(packet,value);
-        if ((result=sendPacket(&packet)))
-            //reuse packet for response
-            result= checkSuccess(&packet);
-    };
-    return result;
+int socketConnected(int *clientSocket) {
+    //declare a variable for keeping getSockOpt error
+    int error_code = 0;
+    //get the size of error code variable
+    socklen_t error_code_size = sizeof(error_code);
+    //getting socket option of a not connected socket return -1
+    int returnValue = getsockopt(*clientSocket, SOL_SOCKET, SO_ERROR, &error_code, &error_code_size);
+    //1 if already connected 0 if not (!-1 is 0)
+    return (returnValue==-1);
 }
 
-int increment(uint32_t name, uint32_t value) {
+int sendPacket(SimpleProtocolPacket* packet) {
     int result=1;
-    if ((result=establishSession(&client_Socket))){
-        //send increment packet
-        SimpleProtocolPacket packet;
-        SPPINIT(packet);
-        SETINC(packet);
-        SETVAR(packet,name);
-        SETVAL(packet,value);
-        if ((result=sendPacket(&packet)))
-            //reuse packet for response
-            result= checkSuccess(&packet);
-    };
+    *packet = SPPTONTW(*packet);
+    if (send(client_Socket,packet,sizeof(SimpleProtocolPacket),0)==-1) {
+        result = 0;
+    }
     return result;
 }
 
-int get(uint32_t name,uint32_t* value) {
-    int result = 1;
-    if ((result=establishSession(&client_Socket))){
-        SimpleProtocolPacket packet;
-        SPPINIT(packet);
-        SETGET(packet);
-        SETVAR(packet,name);
-        if ((result=sendPacket(&packet))) {
-            //reuse packet for response
-            if ((result=checkSuccess(&packet))) {
-                if (GETSUC(packet)) *value = (uint32_t) GETVAL(packet);
-                else result=0;
-            };
-        }
-    };
+int waitResponse(SimpleProtocolPacket *responsePacket) {
+    int result=1;
+    if (recv(client_Socket,responsePacket,sizeof(SimpleProtocolPacket),0)!=-1) {
+        *responsePacket = SPPTOHST(*responsePacket);
+    } else result = 0;
     return result;
 }
+
+int checkSuccess(SimpleProtocolPacket *responsePacket) {
+    int result=1;
+    if (waitResponse(responsePacket)) {
+        if (!GETSUC(*responsePacket)) result=0;
+    }
+    return result;
+}
+
+int initializeToken(uint32_t *token) {
+    *token = 0;
+    int result = 1;
+    FILE* filetoken;
+    if (access(TOKEN_FILE_NAME, F_OK)==0) {
+        filetoken = fopen(TOKEN_FILE_NAME, "r");
+        if (filetoken != NULL) {
+            //read the token from file
+            if (fread(token, sizeof(*token), 1, filetoken) == 0) {
+                //if an error occur (fread return value == 0)
+                result = 0;
+            }
+            fclose(filetoken);
+        }
+    }
+    return result;
+}
+
+int saveTokenToFile(uint32_t token) {
+    int result = 1;
+    FILE* filetoken;
+    if ((filetoken=fopen(TOKEN_FILE_NAME,"w"))!=NULL) {
+        if (fwrite(&token,sizeof(token),1,filetoken)==0) {
+            //if an error occur (fwrite return value == 0)
+            result = 0;
+        }
+    } else result = 0;
+    return result;
+}
+
+/*int getMACaddress(uint8_t * MACaddress) {
+    int status = 1;
+    char buf[256];
+    FILE *fp = fopen("/sys/class/net/eth0/address", "rt");
+    memset(buf, 0, 256);
+    if (fp) {
+        if (fgets(buf, sizeof buf, fp) > 0) {
+            sscanf(buf, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &MACaddress[0],
+                   &MACaddress[1], &MACaddress[2], &MACaddress[3], &MACaddress[4], &MACaddress[5]);
+            status = 0;
+        }
+        fclose(fp);
+    }
+    return status;
+}*/
+
+/**
+ * Check if a given socket is already connected to a server or not.
+ * @param clientSocket int*: the socket descriptor to check
+ * @return 1 if already connected 0 if not
+ */
